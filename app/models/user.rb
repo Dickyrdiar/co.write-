f5d666d927bc7bbb3c766b414246ad61954d92dc
Dicky <nakdeknaknan427@gmail.com>
2018-08-20 07:47:14 +0700

    to create like

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,4 +27,7 @@ class User < ApplicationRecord
 
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
+  has_many :likes 
+  has_many :liked_posts, through: :likes, source: :posts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_15_020155) do
+ActiveRecord::Schema.define(version: 2018_08_20_004620) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -20,6 +20,16 @@ ActiveRecord::Schema.define(version: 2018_08_15_020155) do
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "like_posts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|
