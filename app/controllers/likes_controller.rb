3f867dc36571fc31526bac2d0e5da5f90f61f531
Dicky <nakdeknaknan427@gmail.com>
2018-08-23 19:25:15 +0700

    create like

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,5 +2,12 @@ class LikesController < ApplicationController
     before_action :find_post 
 
     def create 
+        @post.likes.create(user_id: current_user.id)
+        redirect_to post_path(@post)
     end
+
+    private 
+
+    def find_post 
+        @post = Post.find(params[:id])
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -26,6 +26,5 @@ class Post < ApplicationRecord
     has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
     validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
-    has_many :likes 
-
+    has_many :likes
 end
